a1ef89f879d941cf26613410589b1023914e770d
zzid <931010dy@gmail.com>
2020-09-06 18:30:00 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
+
+import { Card } from './component';
+
+import profile_picture from './images/profile_picture.jpeg'
+import title_pic from './images/title.png'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={title_pic} alt='img err' style={{margin: '7vh', width:'38vw'}}></img>
       </header>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <div className="container">
+        <Card/>
+      </div>
     </div>
   );
 }
---
+++ src/component/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Card.module.css'
+const Card = () => {
+    return(
+        <div className={styles.container}>
+            <p>Hello, I'm zzid.</p>
+            <p>DongYun Hwang from Korea, Republic of</p>
+        </div>
+    )
+}
+export default Card;
